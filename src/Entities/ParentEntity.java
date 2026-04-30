504e4e0ed444e205bea4bc7642894d9ebc9544c5
jawah <jawaheralsaadi1@gmail.com>
2026-04-30 14:32:44 +0400

    Step 2 Add The base inheritance and core Entities

---
+++ src/Entities/ParentEntity.java
@@ -0,0 +1,13 @@
+﻿package Entities;
+
+public abstract class ParentEntity {
+
+    //Every item has an ID and a Name.
+    private int id;
+    private String name;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
