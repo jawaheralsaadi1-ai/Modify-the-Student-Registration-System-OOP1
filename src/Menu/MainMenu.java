ef7cf0180766ceba7ccf26bc0184bf6abf0344a5
jawaher <jawaheralsaadi!@gmail.com>
2026-05-03 19:34:13 +0400

    Edit  :  MainMenu script

--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -11,11 +11,11 @@ public class MainMenu implements MenuInterface {
 
     @Override
     public void displayOptions() {
-        System.out.println("1. 🎓 Manage Students");
-        System.out.println("2. 👨‍🏫 Manage Teachers");
-        System.out.println("3. 🏢 Manage Departments");
-        System.out.println("4. 📚 Manage Courses");
-        System.out.println("0. 🚪 Exit System");
+        System.out.println("1.  Manage Students");
+        System.out.println("2.  Manage Teachers");
+        System.out.println("3.  Manage Departments");
+        System.out.println("4.  Manage Courses");
+        System.out.println("0.  Exit System");
         System.out.print("\nSelect a category to continue: ");
     }
 }
