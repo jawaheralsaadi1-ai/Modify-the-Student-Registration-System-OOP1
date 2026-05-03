4d1622e4a2ccc8133f2ac34eadd4daa483f09c2c
jawaher <jawaheralsaadi!@gmail.com>
2026-05-03 16:25:37 +0400

    Step 4 Refactor Entities/ParentEntity to use UUID for unique code

--- src/Entities/ParentEntity.java
+++ src/Entities/ParentEntity.java
@@ -1,4 +1,5 @@
-﻿package Entities;
+﻿/*
+package Entities;
 
 public abstract class ParentEntity {
 
@@ -8,6 +9,21 @@ public abstract class ParentEntity {
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
+*/
+// Refactor
+package Entities;
+import java.util.UUID;//UUID library to ensure every student and teacher has a truly unique code.
+
+public abstract class ParentEntity {
+    // Using UUID makes the ID a unique string
+    private String id = UUID.randomUUID().toString();
+    private String name;
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 }
