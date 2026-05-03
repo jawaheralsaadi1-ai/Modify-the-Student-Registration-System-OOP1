252d5fa759dfa446614031915185e58c16b58943
jawaher <jawaheralsaadi!@gmail.com>
2026-05-03 19:32:28 +0400

    create MenuInterface class for consistent menu behavior

---
+++ src/Menu/MenuInterface.java
@@ -0,0 +1,9 @@
+package Menu;
+
+public interface MenuInterface {
+    // This will print the visual part of the menu on the screen
+    void showMenuHeader();
+
+    // This will display the list of choices (1, 2, 3...)
+    void displayOptions();
+}
