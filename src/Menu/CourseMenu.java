e69cb8ffb5405361de58a4787b3ed9374dfadc83
jawaher <jawaheralsaadi!@gmail.com>
2026-05-04 15:21:59 +0400

    Add Menu class Method for Course and Teacher modules

---
+++ src/Menu/CourseMenu.java
@@ -0,0 +1,12 @@
+package Menu;
+
+public class CourseMenu implements  MenuInterface{
+    @Override
+    public void showMenuHeader() {
+    }
+
+    @Override
+    public void displayOptions() {
+
+    }
+}
---
+++ src/Menu/TeacherMenu.java
@@ -0,0 +1,13 @@
+package Menu;
+
+public class TeacherMenu implements MenuInterface {
+    @Override
+    public void showMenuHeader() {
+
+    }
+
+    @Override
+    public void displayOptions() {
+
+    }
+}
